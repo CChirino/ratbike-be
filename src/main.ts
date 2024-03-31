7a5ec566e73274360397ee9e2041ea8b577b4bc2
CChirino <Christopherchirinosj@gmail.com>
2024-03-30 22:31:13 -0400

    Main.ts archivo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   await app.listen(3000);
 }
 bootstrap();
