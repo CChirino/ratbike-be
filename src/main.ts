a20c57152f9d00df36e4cef6720fe1a86de18eb1
CChirino <Christopherchirinosj@gmail.com>
2024-07-04 23:36:07 -0400

    Cambio en el main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('API Ratbikes')
     .setDescription('Endpoints')
@@ -14,10 +16,43 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors();
+
+  // Configuración de CORS
+  app.enableCors({
+    origin: 'https://ratwave.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  });
+
+  // Middleware para manejar las solicitudes preflight (opciones)
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://ratwave.com');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(204);
+    } else {
+      next();
+    }
+  });
+
+  // Middleware para manejar JSON y URL encoding
   app.use(json({ limit: '20mb' }));
   app.use(urlencoded({ extended: true, limit: '20mb' }));
+
+  // Middleware para servir archivos estáticos
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
+  // Iniciar la aplicación
   await app.listen(3000);
 }
+
 bootstrap();
