83ec1af2fc8d612cda6d2c7cd351e72dcf912c08
CChirino <Christopherchirinosj@gmail.com>
2024-08-19 15:11:26 -0400

    Modificacion de las notificaciones del entorno para produccion

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { TelegramBot } from 'typescript-telegram-bot-api';
+
 @Injectable()
 export class TelegramService {
   private bot: TelegramBot;
   private chatId: string;
   constructor() {
-    // Configura tu bot y el webhook aquí
     const botToken = '7106374452:AAEVtMcQnIbvaiqtUDq3Dcc6brWJOUpTyGo';
     this.chatId = '-4221294991';
 
@@ -13,13 +13,16 @@ export class TelegramService {
   }
 
   async sendErrorMessage(message: string) {
-    try {
-      await this.bot.sendMessage({
-        chat_id: this.chatId,
-        text: message,
-      });
-    } catch (error) {
-      console.error('Failed to send message to Telegram:', error);
+    // Solo envía mensajes si el entorno es producción
+    if (process.env.NODE_ENV === 'production') {
+      try {
+        await this.bot.sendMessage({
+          chat_id: this.chatId,
+          text: message,
+        });
+      } catch (error) {
+        console.error('Failed to send message to Telegram:', error);
+      }
     }
   }
 }
